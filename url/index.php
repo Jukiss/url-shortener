8c1b0930c25e4f441744ec1f0585a715ba1ad8e6
Jukiss <33698745+Jukiss@users.noreply.github.com>
2024-09-19 16:41:40 +0300

    improve visual

--- url/index.php
+++ url/index.php
@@ -8,25 +8,35 @@ $shortener = new UrlShortener();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>URL shortener</title>
 </head>
 <body>
+<div class="container">
     <h1>URL shortener</h1>
+    <div class="form-background">
     <form method="POST">
-        <input type="text" name="url" placeholder="put long URL" required>
+        <input type="text" name="url" placeholder="put your long URL here" required>
         <button type="submit" name="send" >short URL </button>
     </form>
-
+    </div>
     <?php 
    
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['url'])) {//чтобы при обновлении страницы, не генерилась новая ссылка+ защита от долбоеба нужна
-        $shortUrlId = $shortener->shorten($_POST['url']);
-        $shortUrl = "http://localhost/" . $shortUrlId;
-    }
-    //unset($_POST);
-    if (isset($shortUrl)): ?>
-        <p>short link: <a href="<?= $shortUrl ?>"><?= $shortUrl ?></a></p>
-    <?php endif; ?>
+   if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['url'])) {//чтобы при обновлении страницы, не генерилась новая ссылка+ защита от долбоеба нужна
+       $shortUrlId = $shortener->shorten($_POST['url']);
+       $shortUrl = "http://localhost/" . $shortUrlId;
+       //
+       //
+   }
+   //unset($_POST);
+   $_POST['url']='';
+   //header('Location: /url/');
+   if (isset($shortUrl)): ?>
+       <div class="shortlink">
+           <p>short link: <a href="<?= $shortUrl ?>"><?= $shortUrl ?></a></p>
+           </div>
+   <?php endif; ?>
+</div>
+
 </body>
 </html>
